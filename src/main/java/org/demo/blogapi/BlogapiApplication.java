2213fb3cc10c9cc1aa83e46e96d7d3bb040ec5d9
Devendra Sahu <devendra.sahu19@gmail.com>
2023-02-19 09:33:15 +0530

    Postgres setup

---
+++ src/main/java/org/demo/blogapi/BlogapiApplication.java
@@ -0,0 +1,13 @@
+package org.demo.blogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/demo/blogapi/BlogapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
