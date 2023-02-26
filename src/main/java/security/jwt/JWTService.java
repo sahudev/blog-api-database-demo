5f43a8008c0cd93f382a6524bf7e327f558ac81e
Devendra Sahu <devendra.sahu19@gmail.com>
2023-02-27 00:51:13 +0530

    JWT Service is working

---
+++ src/main/java/security/jwt/JWTService.java
@@ -0,0 +1,35 @@
+package security.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.util.Date;
+
+public class JWTService {
+
+    private Algorithm algorithm = Algorithm.HMAC256("SECRET SIGNING KEY (should be in env or config)");
+
+    public String createJWT(Integer userId){
+       return createJWT(
+               userId,
+               new Date(),
+               new Date(System.currentTimeMillis() + 1000*60*60*24*7)
+       );
+    }
+
+    public String createJWT(Integer userId, Date iat, Date exp){
+        String token = JWT.create()
+                .withSubject(userId.toString())
+                .withIssuedAt(iat)
+                .withExpiresAt(exp)
+                .sign(algorithm);
+        return token;
+    }
+
+    public Integer getUserIdFromJWT(String jwt){
+        var verifier = JWT.require(algorithm).build();
+        var decodeJWT = verifier.verify(jwt);
+        var subject = decodeJWT.getSubject();
+        return Integer.parseInt(subject);
+    }
+}
---
+++ src/test/java/org/demo/blogapi/security/jwt/JWTServiceTests.java
@@ -0,0 +1,28 @@
+package org.demo.blogapi.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import security.jwt.JWTService;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JWTServiceTests {
+
+    private JWTService jwtService = new JWTService();
+
+    @Test
+    void canCreateJWTFromUserId(){
+        var userId = 1122;
+        var jwt = jwtService.createJWT(userId,new Date(1677438053), new Date(1678042853));
+        assertEquals("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMTIyIiwiZXhwIjoxNjc4MDQyLCJpYXQiOjE2Nzc0Mzh9.s841mgHASlw6yobS6PWQMdDrU5RMnwGt_bNIfGrpvYU",jwt);
+    }
+
+    @Test
+    void canVerifyJWT(){
+        var jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMTIyIiwiZXhwIjoxNjc4MDQyODUzLCJpYXQiOjE2Nzc0MzgwNTN9.ITEZidF7x9PcqoikODEdNB-ZJgREfpWGQUW_Wc5UOMk";
+        var userId = jwtService.getUserIdFromJWT(jwt);
+        assertEquals(1122,userId);
+    }
+
+}
